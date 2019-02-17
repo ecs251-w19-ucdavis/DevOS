10e1039022d0f17494f37e39c10794a576e99a8f
devsam19 <sanjat.mishra@outlook.com>
2019-02-16 17:07:52 -0800

    Added and Renamed Folders

--- src/index.js
+++ src/index.js
@@ -3,32 +3,9 @@ var fs = require('fs');
 var express = require('express');
 
 
-
-// For File Upload
-
-const fileUpload = require('express-fileupload');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-
-app.use(fileUpload());
-
-app.post('/upload', function(req,res){
-    if(Object.keys(req.files).length == 0){
-        return res.status(400).send('No Files were uploaded');
-    }
-
-    let sampleFile = req.files.sampleFile;
-
-    sampleFile.mv('', function(err) {
-        if (err)
-          return res.status(500).send(err);
-    
-        res.send('File uploaded!');
-
-
-})
-
 
 app.get('/home',function(req,res){
     res.type('text/plain');
@@ -53,7 +30,7 @@ app.use(function(req,res,next){
 })
 
 
-app.listen(app.get('port'),'10.0.0.170', function(){
+app.listen(app.get('port'),'10.0.0.188', function(){
     console.log('Express started on http://localhost:' +
     app.get('port') + ' ; press Ctrl-C to terminate.' );
 });
