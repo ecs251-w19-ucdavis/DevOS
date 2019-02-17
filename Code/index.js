60d2006649670c1c6a4e55ab035e1f3914eeb925
devsam19 <sanjat.mishra@outlook.com>
2019-02-16 16:37:10 -0800

    Added Folders

--- Code/index.js
+++ Code/index.js
@@ -13,6 +13,23 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(fileUpload());
 
+app.post('/upload', function(req,res){
+    if(Object.keys(req.files).length == 0){
+        return res.status(400).send('No Files were uploaded');
+    }
+
+    let sampleFile = req.files.sampleFile;
+
+    sampleFile.mv('', function(err) {
+        if (err)
+          return res.status(500).send(err);
+    
+        res.send('File uploaded!');
+
+
+})
+
+
 app.get('/home',function(req,res){
     res.type('text/plain');
     res.send('Sanjats Page');
