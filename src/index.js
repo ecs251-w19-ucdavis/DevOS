0092915889372d9454bad5a9de55ac2ed948df3b
Kavish Doshi <kavishdoshi28@gmail.com>
2019-02-17 00:40:55 -0800

    File Upload Part 1

--- src/index.js
+++ src/index.js
@@ -1,36 +1,61 @@
 var server = require('http');
 var fs = require('fs');
-var express = require('express');
+var express	=	require("express");
+var multer	=	require('multer');
+var app	=	express();
+
+//For file upload - if you run from the DevOS folder like "node src/index.js" make no changes
+//For file upload - if you run from the src folder change destination-callback to "./uploads" from "./src/uploads"
+var storage	=	multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './src/uploads');  
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
 
 
-var app = express();
+//Part of the Node JS to navigate through pages
 
+//Setting the port number to listen to the request
 app.set('port', process.env.PORT || 3000);
 
+
+//For getting to the home page
 app.get('/home',function(req,res){
-    res.type('text/plain');
-    res.send('Sanjats Page');
+  res.type('text/plain');
+  res.send('Hello World');
 });
 
-
-//Custom 404 Page
-
-app.use(function(req,res,next){
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Page not found');
+//Post request of the file upload
+app.post('/api',function(req,res){
+	upload(req,res,function(err) {
+		if(err) {
+			return res.end("Error uploading file.");
+		}
+		res.end("File is uploaded");
+	});
 });
 
-//Custom 500 Page
+//Custom 404 Page - Error Page
 
 app.use(function(req,res,next){
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Internal Server Error');
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Page not found');
+});
+
+//Custom 500 Page - Error page
+
+app.use(function(req,res,next){
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Internal Server Error');
 })
 
-
-app.listen(app.get('port'),'10.0.0.188', function(){
-    console.log('Express started on http://localhost:' +
-    app.get('port') + ' ; press Ctrl-C to terminate.' );
+//Change the IP Address here according 
+app.listen(app.get('port'),'10.0.0.170', function(){
+  console.log('Express started on http://localhost:' +
+  app.get('port') + ' ; press Ctrl-C to terminate.' );
 });
