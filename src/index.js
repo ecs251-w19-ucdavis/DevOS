fa0876268a400f1ca7a7e18ce1856d257754b792
devsam19 <sanjat.mishra@outlook.com>
2019-02-18 13:06:28 -0800

    Added Client and Server Socket

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-var server = require('http');
+var http = require('http');
 var fs = require('fs');
 var express = require('express');
-
+var socket = require('socket.io');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
 
-app.get('/home',function(req,res){
-    res.type('text/plain');
-    res.send('Sanjats Page');
+//Printing all commandline arguments
+process.argv.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+  });
+
+  app.set('port', process.env.PORT || process.argv[2]);
+
+  var server = app.listen(app.get('port'),'10.0.0.188', function(){
+    console.log('Express started on http://localhost:' +
+    app.get('port') + ' ; press Ctrl-C to terminate.' );
 });
+
+var io = socket.listen(server);
+
+
+//Socket setup
+
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/views/main.html')
+    console.log('Just rendered!');
+});
+
+
+//Socket Connection
+
+
+io.on('connection',function(socket){
+    console.log('A User made a socket connection',socket.id);
+})
+
+
 
 
 //Custom 404 Page
@@ -30,7 +57,3 @@ app.use(function(req,res,next){
 })
 
 
-app.listen(app.get('port'),'10.0.0.188', function(){
-    console.log('Express started on http://localhost:' +
-    app.get('port') + ' ; press Ctrl-C to terminate.' );
-});
